fa58f8b02547cd1102b1844da159917bafa68bd2
shelepuginivan <shelepuginivanm@gmail.com>
2023-08-27 12:02:26 +0300

    feat(theme): make git icons transparent

--- lua/core/theme.lua
+++ lua/core/theme.lua
@@ -5,11 +5,29 @@ local ok, _ = pcall(vim.cmd, 'colorscheme ' .. colorscheme)
 vim.o.background = 'dark'
 if not ok then
     vim.notify('Colorscheme ' .. colorscheme .. ' not found!')
-    return
 end
+
+local extra_groups = {
+    'NeoTreeNormal',
+    'NeoTreeNormalNC',
+    'NeoTreeFileIcon',
+    'NeoTreeFileName',
+    'NeoTreeDirectoryIcon',
+    'NeoTreeDirectoryName',
+    'NeoTreeGitAdded',
+    'NeoTreeGitConflict',
+    'NeoTreeGitDeleted',
+    'NeoTreeGitIgnored',
+    'NeoTreeGitModified',
+    'NeoTreeGitRenamed',
+    'NeoTreeGitStaged',
+    'NeoTreeGitUnstaged',
+    'NeoTreeGitUntracked',
+}
 
 vim.o.termguicolors = true
 vim.g.transparent_enabled = true
+vim.g.transparent_groups = vim.list_extend(vim.g.transparent_groups or {}, extra_groups)
 
 vim.cmd('highlight DiagnosticError guifg=Red')
 vim.cmd('highlight DiagnosticWarn guifg=#ffe533')
