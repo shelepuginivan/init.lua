f9112eb6cd64fe088799da90dab8bf32c42f0621
shelepuginivan <shelepuginivanm@gmail.com>
2023-08-27 05:10:02 +0300

    feat(core): add settings module

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 require('core.mappings')
 require('core.plugins')
+require('core.settings')
 require('core.theme')
 
 require('plugins.autosave')
---
+++ lua/core/settings.lua
@@ -0,0 +1,15 @@
+local opt = vim.opt
+
+opt.mouse = 'a'
+opt.encoding = 'utf-8'
+opt.fileformat = 'unix'
+opt.number = true
+opt.swapfile = false
+opt.scrolloff = 7
+
+opt.expandtab = true
+opt.copyindent = true
+opt.softtabstop = 4
+opt.shiftwidth = 4
+opt.tabstop = 4
+
