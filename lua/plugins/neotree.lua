8757d965f53f72544b991b6e990775224ba8729c
shelepuginivan <shelepuginivanm@gmail.com>
2023-08-27 12:39:34 +0300

    feat(neotree): change default plugin settings

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,10 +1,31 @@
 require('neo-tree').setup({
     close_if_last_window = true,
+    default_component_configs = {
+        git_status = {
+            symbols = {
+                untracked = '?',
+                ignored = '',
+                unstaged = '󰄱',
+                staged = '',
+                conflict = '!',
+            },
+        },
+        icon = {
+            folder_closed = '',
+            folder_open = '',
+            folder_empty = '󰜌',
+            default = '*',
+            highlight = 'NeoTreeFileIcon',
+        },
+    },
     filesystem = {
         filtered_items = {
             visible = true,
             hide_dotfiles = false,
             hide_gitignored = true,
         },
+    },
+    window = {
+        width = 30,
     },
 })
