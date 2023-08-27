4df3bebbacca660e9f9e4c5125e53f3b3058aa8b
shelepuginivan <shelepuginivanm@gmail.com>
2023-08-27 07:25:37 +0300

    feat(plugins): add startup plugin configuration

---
+++ lua/plugins/startup.lua
@@ -0,0 +1,68 @@
+local neovim = {
+    '╔══════════════════════════════════════════════════════╗',
+    '                                                        ',
+    '   ▀█▄   ▀█▀                           ██               ',
+    '    █▀█   █    ▄▄▄▄    ▄▄▄   ▄▄▄▄ ▄▄▄ ▄▄▄  ▄▄ ▄▄ ▄▄     ',
+    '    █ ▀█▄ █  ▄█▄▄▄██ ▄█  ▀█▄  ▀█▄  █   ██   ██ ██ ██    ',
+    '    █   ███  ██      ██   ██   ▀█▄█    ██   ██ ██ ██    ',
+    '   ▄█▄   ▀█   ▀█▄▄▄▀  ▀█▄▄█▀    ▀█    ▄██▄ ▄██ ██ ██▄   ',
+    '                                                        ',
+    '╚══════════════════════════════════════════════════════╝'
+}
+
+local quote = { 'Will there ever be a place for the broken in the light?' }
+
+require('startup').setup({
+    header = {
+        type = 'text',
+        oldfiles_directory = false,
+        align = 'center',
+        fold_section = false,
+        title = 'Header',
+        margin = 5,
+        content = neovim,
+        highlight = 'Text',
+        default_color = '',
+        oldfiles_amount = 0
+    },
+    body = {
+        type = 'mapping',
+        oldfiles_directory = false,
+        align = 'center',
+        fold_section = false,
+        title = 'Basic Commands',
+        margin = 5,
+        content = {
+            { ' Find File', 'Telescope find_files', '<leader>ff' },
+            { ' Find Word', 'Telescope live_grep', '<leader>lg' },
+            { ' Settings', 'e ~/.config/nvim/init.lua', 'cc'},
+            { ' Quit nvim', 'qa', 'qq'}
+        },
+        highlight = 'String',
+        default_color = '',
+        Oldfiles_amount = 0
+    },
+    footer = {
+        type = 'text',
+        oldfiles_directory = false,
+        align = 'center',
+        fold_section = false,
+        title = 'Footer',
+        margin = 5,
+        content = quote,
+        highlight = 'Number',
+        default_color = '',
+        oldfiles_amount = 0
+    },
+    options = {
+        mapping_keys = true,
+        cursor_column = 0.5,
+        empty_lines_between_mappings = true,
+        disable_statuslines = true,
+        paddings = { 2, 2, 1, 2 }
+    },
+    mappings = {
+        execute_command = '<CR>'
+    },
+    parts = { 'header', 'body', 'footer' }
+})
