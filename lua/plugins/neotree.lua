1917ab95a21cf977675212be5fe4ad437d8939a0
shelepuginivan <shelepuginivanm@gmail.com>
2023-08-27 04:40:15 +0300

    feat(plugins): add neotree config

---
+++ lua/plugins/neotree.lua
@@ -0,0 +1,9 @@
+require('neotree').setup({
+    filesystem = {
+        filtered_items = {
+            visible = true,
+            hide_dotfiles = false,
+            hide_gitignored = true
+        }
+    }
+})
