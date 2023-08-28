8ae34cd28fc11fdb87318c2bcfdacf8d95ab4b77
shelepuginivan <shelepuginivanm@gmail.com>
2023-08-29 00:42:42 +0300

    feat(mappings, lsp): update LSP mappings
    Set LSP mappings when language server is attached, also replace
    deprecated `nvim-lspconfig` functions with newer ones

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -1,4 +1,4 @@
-local map = vim.api.nvim_set_keymap
+local map = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
 vim.g.mapleader = ' '
@@ -21,22 +21,35 @@ map('v', "'", "di''<left><esc>p", opts)
 map('v', '"', 'di""<left><esc>p', opts)
 map('v', '`', 'di``<left><esc>p', opts)
 
--- LSP mappings
--- See :help vim.lsp.buf.*
-map('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-map('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-map('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-map('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-map('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-map('n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-map('n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-map('n', '<leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-map('n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-map('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-map('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-map('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-map('n', '<leader>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-map('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-map('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-map('n', '<leader>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
-map('n', '<leader>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+-- Set LSP mappings when language server is attached
+-- See `:help vim.lsp.*` for documentation on any of the below functions
+local on_attach = function(event)
+    local opts = { buffer = event.buf }
+
+    map('n', 'gD', vim.lsp.buf.declaration, opts)
+    map('n', 'gd', vim.lsp.buf.definition, opts)
+    map('n', 'K', vim.lsp.buf.hover, opts)
+    map('n', 'gi', vim.lsp.buf.implementation, opts)
+    map('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+    map('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
+    map('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
+    map('n', '<leader>wl', function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, opts)
+    map('n', '<leader>D', vim.lsp.buf.type_definition, opts)
+    map('n', '<leader>rn', vim.lsp.buf.rename, opts)
+    map('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+    map('n', 'gr', vim.lsp.buf.references, opts)
+    map('n', '<leader>e', vim.diagnostic.open_float, opts)
+    map('n', '[d', vim.diagnostic.goto_prev, opts)
+    map('n', ']d', vim.diagnostic.goto_next, opts)
+    map('n', '<leader>q', vim.diagnostic.setloclist, opts)
+    map('n', '<leader>f', function()
+        vim.lsp.buf.format({ async = true })
+    end, opts)
+end
+
+vim.api.nvim_create_autocmd('LspAttach', {
+    group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+    callback = on_attach,
+})
