b08eb43516d9b84d632870d21d0cd58ff01450f2
shelepuginivan <shelepuginivanm@gmail.com>
2023-08-29 02:56:49 +0300

    refactor(theme): add colors table

--- lua/core/theme.lua
+++ lua/core/theme.lua
@@ -1,5 +1,18 @@
 local colorscheme = 'gruvbox'
 
+local colors = {
+    gray = '#32302f',
+    gray_darker = '#282828',
+    gray_lighter = '#3c3836',
+    beige = '#ebdbb2',
+    beige_lighter = '#f9f5d7',
+    red = '#fb4934',
+    orange = '#d65d0e',
+    yellow = '#fabd2f',
+    green = '#98971a',
+    blue = '#458588',
+}
+
 local extra_groups = {
     'NeoTreeNormal',
     'NeoTreeNormalNC',
@@ -18,26 +31,27 @@ local extra_groups = {
     'NeoTreeGitUntracked',
 }
 
+-- Global
 vim.cmd.colorscheme(colorscheme)
 vim.o.termguicolors = true
 vim.g.transparent_enabled = true
 vim.g.transparent_groups = vim.list_extend(vim.g.transparent_groups or {}, extra_groups)
 
 -- Telescope
-vim.api.nvim_set_hl(0, 'TelescopeSelection', { bg = '#32302f', fg = '#fb4934' })
-vim.api.nvim_set_hl(0, 'TelescopeMatching', { fg = '#fabd2f' })
-vim.api.nvim_set_hl(0, 'TelescopeNormal', { bg = '#32302f' })
-vim.api.nvim_set_hl(0, 'TelescopeResultsBorder', { bg = '#32302f', fg = '#32302f' })
-vim.api.nvim_set_hl(0, 'TelescopeResultsTitle', { bg = '#cc241d', fg = '#ebdbb2' })
-vim.api.nvim_set_hl(0, 'TelescopePromptNormal', { bg = '#3c3836' })
-vim.api.nvim_set_hl(0, 'TelescopePromptBorder', { bg = '#3c3836', fg = '#3c3836' })
-vim.api.nvim_set_hl(0, 'TelescopePromptTitle', { bg = '#d65d0e', fg = '#ebdbb2' })
-vim.api.nvim_set_hl(0, 'TelescopePreviewNormal', { bg = '#282828' })
-vim.api.nvim_set_hl(0, 'TelescopePreviewBorder', { bg = '#282828', fg = '#282828' })
-vim.api.nvim_set_hl(0, 'TelescopePreviewTitle', { bg = '#98971a', fg = '#ebdbb2' })
+vim.api.nvim_set_hl(0, 'TelescopeSelection', { bg = colors.gray, fg = colors.red })
+vim.api.nvim_set_hl(0, 'TelescopeMatching', { fg = colors.yellow })
+vim.api.nvim_set_hl(0, 'TelescopeNormal', { bg = colors.gray })
+vim.api.nvim_set_hl(0, 'TelescopeResultsBorder', { bg = colors.gray, fg = colors.gray })
+vim.api.nvim_set_hl(0, 'TelescopeResultsTitle', { bg = colors.red, fg = colors.beige })
+vim.api.nvim_set_hl(0, 'TelescopePromptNormal', { bg = colors.gray_lighter })
+vim.api.nvim_set_hl(0, 'TelescopePromptBorder', { bg = colors.gray_lighter, fg = colors.gray_lighter })
+vim.api.nvim_set_hl(0, 'TelescopePromptTitle', { bg = colors.orange, fg = colors.beige })
+vim.api.nvim_set_hl(0, 'TelescopePreviewNormal', { bg = colors.gray_darker })
+vim.api.nvim_set_hl(0, 'TelescopePreviewBorder', { bg = colors.gray_darker, fg = colors.gray_darker })
+vim.api.nvim_set_hl(0, 'TelescopePreviewTitle', { bg = colors.green, fg = colors.beige })
 
 -- LSP Diagnostic messages
-vim.cmd('highlight DiagnosticError guifg=Red')
-vim.cmd('highlight DiagnosticWarn guifg=#ffe533')
-vim.cmd('highlight DiagnosticInfo guifg=#2eccfe')
-vim.cmd('highlight DiagnosticHint guifg=White')
+vim.api.nvim_set_hl(0, 'DiagnosticError', { fg = colors.red })
+vim.api.nvim_set_hl(0, 'DiagnosticWarn', { fg = colors.yellow })
+vim.api.nvim_set_hl(0, 'DiagnosticInfo', { fg = colors.blue })
+vim.api.nvim_set_hl(0, 'DiagnosticHine', { fg = colors.beige_lighter })
