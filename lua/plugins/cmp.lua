c2d2276f89d0bf5de8bb54f2abd8a259f902a288
shelepuginivan <shelepuginivanm@gmail.com>
2023-12-31 00:57:25 +0300

    feat(plugins, cmp): setup nvim-cmp plugin

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,28 @@
+local cmp = require('cmp')
+local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
+
+cmp.setup({
+    snippet = {
+        expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+        end,
+    },
+    mapping = cmp.mapping.preset.insert({
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    }),
+    sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'nvim_lua' },
+        { name = 'luasnip' },
+    }, {
+        { name = 'buffer' },
+        { name = 'path' },
+    }),
+})
+
+cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
