f851cb3cc8dd5feff7fc7d7aa8c467d2009c487b
shelepuginivan <shelepuginivanm@gmail.com>
2024-01-09 18:41:02 +0300

    feat: enhance installation script

--- install.sh
+++ install.sh
@@ -1,19 +1,25 @@
-#!/usr/bin/env bash
-set -e
+#!/bin/sh
 
 CONFIG_PATH="$HOME/.config/nvim"
-SHARED_PATH="$HOME/.local/share/nvim"
+DATA_PATH="$HOME/.local/share/nvim"
+REPOSITORY="https://github.com/shelepuginivan/init.lua.git"
 
-echo "Backing up previous installation..."
+# Check if there is an existing configuration
 if [ -d "$CONFIG_PATH" ]; then
+    echo "Found existing configuration!"
+    echo "Backing up to $CONFIG_PATH.bak..."
     mv "$CONFIG_PATH" "$CONFIG_PATH.bak"
+    echo "Done!"
 fi
 
-if [ -d "$SHARED_PATH" ]; then
-    mv "$SHARED_PATH" "$SHARED_PATH.bak"
+# Check if there is a directory with Neovim data
+if [ -d "$DATA_PATH" ]; then
+    echo "Backing up Neovim data..."
+    mv "$DATA_PATH" "$DATA_PATH.bak"
+    echo "Done!"
 fi
 
 echo "Cloning the repository..."
-git clone --depth 1 https://github.com/shelepuginivan/init.lua "$CONFIG_PATH"
+git clone --depth 1 "$REPOSITORY" "$CONFIG_PATH"
 
 echo "Installation compete!"
