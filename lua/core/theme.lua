72f57e7581767f657246c3fb003ae25a50f2b5bc
shelepuginivan <shelepuginivanm@gmail.com>
2024-08-03 15:09:05 +0300

    feat(theme): optimize and enhance theme

--- lua/core/theme.lua
+++ lua/core/theme.lua
@@ -4,6 +4,7 @@ local colors = {
     gray = '#32302f',
     gray_darker = '#282828',
     gray_lighter = '#3c3836',
+    gray_lightest = '#928374',
     beige = '#ebdbb2',
     beige_lighter = '#f9f5d7',
     red = '#fb4934',
@@ -11,6 +12,7 @@ local colors = {
     yellow = '#fabd2f',
     green = '#98971a',
     blue = '#458588',
+    purple = '#b16286',
 }
 
 local extra_groups = {
@@ -31,7 +33,7 @@ local extra_groups = {
     'NeoTreeGitUntracked',
 }
 
-local function highlight(name, val)
+local function hl(name, val)
     vim.api.nvim_set_hl(0, name, val)
 end
 
@@ -40,29 +42,34 @@ vim.cmd.colorscheme(colorscheme)
 vim.o.termguicolors = true
 vim.g.transparent_enabled = true
 vim.g.transparent_groups = vim.list_extend(vim.g.transparent_groups or {}, extra_groups)
-vim.api.nvim_set_hl(0, 'NormalFloat', { bg = colors.gray, fg = colors.beige })
+
+hl('@comment', { fg = colors.gray_lightest, italic = true })
+hl('NormalFloat', { bg = colors.gray, fg = colors.beige })
+
+-- Go
+hl('goPackageComment', { fg = colors.gray_lightest, bold = true })
 
 -- nvim-cmp
-highlight('CmpItemAbbrDeprecated', { fg = '#808080', strikethrough = true })
-highlight('CmpItemAbbrMatch', { fg = colors.red })
-highlight('CmpItemAbbrMatchFuzzy', { fg = colors.orange })
-highlight('CmpItemKind', { fg = colors.yellow })
+hl('CmpItemAbbrDeprecated', { fg = '#808080', strikethrough = true })
+hl('CmpItemAbbrMatch', { fg = colors.red })
+hl('CmpItemAbbrMatchFuzzy', { fg = colors.orange })
+hl('CmpItemKind', { fg = colors.yellow })
 
 -- Telescope
-highlight('TelescopeSelection', { bg = colors.gray, fg = colors.red })
-highlight('TelescopeMatching', { fg = colors.yellow })
-highlight('TelescopeNormal', { bg = colors.gray })
-highlight('TelescopeResultsBorder', { bg = colors.gray, fg = colors.gray })
-highlight('TelescopeResultsTitle', { bg = colors.red, fg = colors.beige })
-highlight('TelescopePromptNormal', { bg = colors.gray_lighter })
-highlight('TelescopePromptBorder', { bg = colors.gray_lighter, fg = colors.gray_lighter })
-highlight('TelescopePromptTitle', { bg = colors.orange, fg = colors.beige })
-highlight('TelescopePreviewNormal', { bg = colors.gray_darker })
-highlight('TelescopePreviewBorder', { bg = colors.gray_darker, fg = colors.gray_darker })
-highlight('TelescopePreviewTitle', { bg = colors.green, fg = colors.beige })
+hl('TelescopeSelection', { bg = colors.gray, fg = colors.red })
+hl('TelescopeMatching', { fg = colors.yellow })
+hl('TelescopeNormal', { bg = colors.gray })
+hl('TelescopeResultsBorder', { bg = colors.gray, fg = colors.gray })
+hl('TelescopeResultsTitle', { bg = colors.red, fg = colors.beige })
+hl('TelescopePromptNormal', { bg = colors.gray_lighter })
+hl('TelescopePromptBorder', { bg = colors.gray_lighter, fg = colors.gray_lighter })
+hl('TelescopePromptTitle', { bg = colors.orange, fg = colors.beige })
+hl('TelescopePreviewNormal', { bg = colors.gray_darker })
+hl('TelescopePreviewBorder', { bg = colors.gray_darker, fg = colors.gray_darker })
+hl('TelescopePreviewTitle', { bg = colors.green, fg = colors.beige })
 
 -- LSP Diagnostic messages
-highlight('DiagnosticError', { fg = colors.red })
-highlight('DiagnosticWarn', { fg = colors.yellow })
-highlight('DiagnosticInfo', { fg = colors.blue })
-highlight('DiagnosticHine', { fg = colors.beige_lighter })
+hl('DiagnosticError', { fg = colors.red })
+hl('DiagnosticWarn', { fg = colors.yellow })
+hl('DiagnosticInfo', { fg = colors.blue })
+hl('DiagnosticHine', { fg = colors.beige_lighter })
