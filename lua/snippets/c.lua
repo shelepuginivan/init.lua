46bec696403cb0c08b40cc95cf7ae80a4513592d
shelepuginivan <shelepuginivanm@gmail.com>
2024-09-08 18:46:04 +0300

    feat(snippets, c): add more snippets

--- lua/snippets/c.lua
+++ lua/snippets/c.lua
@@ -5,11 +5,37 @@ local i = ls.insert_node
 
 ls.add_snippets('c', {
     s({
+        trig = 'define',
+        name = '#define directive',
+    }, {
+        t('#define '),
+        i(1, 'MACRO_NAME'),
+        t(' '),
+        i(2, 'value'),
+    }),
+    s({
         trig = 'include',
-        name = 'Include directive',
+        name = '#include directive',
     }, {
         t('#include <'),
         i(1, 'stdio.h'),
         t('>'),
+    }),
+    s({
+        trig = 'main',
+        name = 'main function',
+    }, {
+        t('int main('),
+        i(1, 'int argc, char *argv[]'),
+        t({
+            ')',
+            '{',
+            '\t',
+        }),
+        i(2),
+        t({
+            '',
+            '}',
+        }),
     }),
 })
